ef52bb1845b74d9fc5066eacd4a0f8b83bfbed7b
VincentOng12 <88222523+VincentOng12@users.noreply.github.com>
2021-11-09 21:02:46 +0700

    create counselor router endpoints and counselor controller

---
+++ controllers/counselorController.js
@@ -0,0 +1,63 @@
+const {Counselor} = require("../models")
+
+class CounselorController{
+    static async getCounselor(req,res,next){
+        try {
+            const counselors = await Counselor.findAll()
+            res.status(200).json(counselors)
+        } catch (err) {
+            console.log(err, 'err get counselor')
+        }
+    }
+
+    static async getCounselorById(req,res,next){
+        try {
+            const {id} = req.params
+            const counselor = await Counselor.findOne({
+                where: {
+                    id: id
+                }
+            })
+            if(!counselor){
+                throw {name: "counselorNotFound"}
+            }
+            res.status(200).json(counselor)
+        } catch (err) {
+            console.log(err, 'err get counselor by id')
+            if(err.name === "counselorNotFound"){
+                res.status(400).json({message: "Counselor not found!"})
+            }
+        }
+    }
+
+    static async createCounselor(req,res,next){
+        try {
+            const {} = req.body
+            const created = await Counselor.create({
+
+            })
+            res.status(201).json(created)
+        } catch (err) {
+            console.log(err, 'err create counselor')
+        }
+    }
+
+    static async updateCounselor(req,res,next){
+        try {
+            const {} = req.body
+            const {id} = req.params
+            // find one dulu
+            const updated = await Counselor.update({
+
+            },{
+                where:{
+                    id:id
+                }
+            })
+
+        } catch (err) {
+            console.log(err, 'err update counselor')
+        }
+    }
+}
+module.exports = CounselorController
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const counselor = require("./counselorRoute")
+
+router.get('/', (req,res)=>{
+    res.send('hello cuy')
+})
+
+router.use("/counselor", counselor)
 
 module.exports = router;
