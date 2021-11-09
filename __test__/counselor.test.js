ca1187bc1c2864c59a33af8bd4fd095b9732c567
VincentOng12 <88222523+VincentOng12@users.noreply.github.com>
2021-11-09 22:16:19 +0700

    initial setup counselor test

---
+++ __test__/counselor.test.js
@@ -0,0 +1,32 @@
+const app = require("../app");
+const request = require("supertest");
+const {sequelize} = require("../models");
+const { queryInterface } = sequelize;
+
+
+describe("Counserlor Routes Test", ()=>{
+    describe("GET /counselor - get all counselor",  ()=>{
+        // test("200 Success - should get all counselor", (done)=>{
+
+        // })
+        // Kondisi yg error gmn
+    })
+    describe("GET /counselor/:id - get counselor by id",  ()=>{
+        // test("200 Success - should get one matching counselor", (done)=>{
+
+        // })
+        // Kondisi yg error gmn
+    })
+    describe("POST /counselor - create new counselor",  ()=>{
+        // test("201 Success create counselor", (done)=>{
+
+        // })
+        // Kondisi yg error gmn
+    })
+    describe("PUT/counselor - update counselor field",  ()=>{
+        // test("200 Success update counselor", (done)=>{
+
+        // })
+        // Kondisi yg error gmn
+    })
+})
