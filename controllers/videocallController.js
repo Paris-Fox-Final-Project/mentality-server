cf9300190ee1b4b82d37d8e5030c28f7f2c33e11
VincentOng12 <88222523+VincentOng12@users.noreply.github.com>
2021-11-14 21:27:59 +0700

    added some comment on video controller

--- controllers/videocallController.js
+++ controllers/videocallController.js
@@ -1,8 +1,26 @@
 const axios = require("axios")
-const dailyKey = "a3fdbd0c05718b0604ccb0aadd96108c7990be061e67ba7a482630907364036f"
+const dailyKey = process.env.DAILY_API_KEY
 
 const rumus = Math.floor((Date.now()/1000)) + 86400 //'exp' : 
 
+// atur gk bisa join sebelum waktunya
+// nbf: integer
+// nama room mau beda" atau sama saja
+// harus ada kolom buat simpen link room buat user dan counselor ketika room dibuat?
+/**
+ * flow
+ * sukses bayar bikin room
+ * room link simen ke table user ?(di tabel user aj mungkin simpennya, berarti ada patch)
+ * 
+ */
+
+
+/**
+ * bayar done
+ * bikin room
+ * di fungsi bikin room kalo udh sukses bikin room langsung update ke counselorUser by id masukin link meeting
+ * pas bikin room yg diatur tambah nbf mungkin dan exp
+ */
 class VideocallController{
     static async createRoom(req,res,next){
         // res.send("bikin video")
@@ -15,13 +33,14 @@ class VideocallController{
                     authorization: 'Bearer ' + dailyKey
                 },
                 data: { 
-                    name: "mentality",
-                    privacy: "private",
+                    // name: "mentality",
+                    privacy: "public",
                     properties: {
-                        "start_audio_off":false,
+                        "start_audio_off":true,
                         "start_video_off":true,
                         "max_participants": 2,
-                        "enable_prejoin_ui": true
+                        // "enable_prejoin_ui": true,
+                        "hide_daily_branding": true
                     }
                  }
             })
@@ -35,7 +54,7 @@ class VideocallController{
         // res.send("join to room")
         try {
             const join = await axios({
-                url: "https://api.daily.co/v1/rooms/DPZRaQZjpAMdMw4rxj9R",
+                url: "https://api.daily.co/v1/rooms/mentality",
                 method: "GET",
                 headers:{
                     'content-type': 'application/json',
